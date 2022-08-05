448311fff7f175c37e232cdcbc148ebf5f9356e6
Joseadu <jose.uk94@gmail.com>
2022-08-05 16:17:23 +0200

    Integrar menú hamburguesa; Añadir media queries para que los distintos menús no salgan en la versión que no les corresponde

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.getElementById('navbarEmail');
 const desktopMenu = document.getElementById('desktopMenu');
+const menuHamIcon = document.getElementById('hamIcon');
+const mobileMenu = document.getElementById('mobileMenu');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
 }
