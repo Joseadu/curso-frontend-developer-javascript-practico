3ece9b4b9d9311485fce6665ad91c1a13d43cf82
Joseadu <jose.uk94@gmail.com>
2022-08-05 17:06:25 +0200

    Integrar carrito de la compra; Hacer que si un menú/carrito está abierto, el resto se cierre

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ const navbarEmail = document.getElementById('navbarEmail');
 const desktopMenu = document.getElementById('desktopMenu');
 const menuHamIcon = document.getElementById('hamIcon');
 const mobileMenu = document.getElementById('mobileMenu');
+const productDetail = document.getElementById('productDetail');
+const navbarShoppingCart = document.getElementById('navbarShoppingCart');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+navbarShoppingCart.addEventListener('click', toggleNavbarShoppingCart);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function toggleMobileMenu () {
     mobileMenu.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetail.classList.add('inactive');
+}
+
+function toggleNavbarShoppingCart () {
+    productDetail.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 }
