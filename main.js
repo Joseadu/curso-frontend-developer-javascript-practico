abe4fca12ed8835f3317307b22508d229ba8f0cb
Joseadu <jose.uk94@gmail.com>
2022-08-10 10:44:36 +0200

    Hasta ahora si quería meter un nuevo producto tenía que hacerlo uno por uno en el HTML. En esta actualización los productos se añaden desde el archivo main.js con un push a un array que guarda los productos y los muestra en el DOM con un ciclo for.

--- main.js
+++ main.js
@@ -26,3 +26,95 @@ function toggleNavbarShoppingCart () {
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
 }
+
+
+// CREAR LISTA DE PRODUCTOS AUTOMATICAMENTE
+
+const productList = [];
+
+productList.push({
+    name: 'Portátil',
+    price: '1000',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Teclado',
+    price: '70',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Ratón',
+    price: '50',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+
+// GENERAR PRODUCTOS AUTOMÁTICAMENTE
+
+// for (product of productList) {
+
+//     const htmlCards = 
+//     `
+//     <div class="product-card">
+//         <img src=${product.image} alt="">
+//         <div class="product-info">
+//           <div>
+//             <p>${product.price}</p>
+//             <p>${product.name}</p>
+//           </div>
+
+//           <figure>
+//             <img src="./icons/bt_add_to_cart.svg" alt="">
+//           </figure>
+//         </div>
+//       </div>
+//     `
+
+//     const cardsContainer = document.getElementById('cardsContainer');
+//     cardsContainer.innerHTML += htmlCards;
+// }
+
+
+for (product of productList) {
+
+    // Crear todos los atributos necesarios añadiéndoles su clase necesaria y previamente maquetadas en CSS
+
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productCardImg = document.createElement('img');
+    productCardImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDescription = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    const productInfoFigure = document.createElement('figure');
+
+    const productInfoFigureImg = document.createElement('img');
+    productInfoFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+
+
+    // Ahora hay que asignarles a cada elemento su contenedor padre
+
+    productInfoFigure.appendChild(productInfoFigureImg);
+
+    productInfoDescription.append(productPrice, productName);
+
+    productInfo.append(productInfoDescription, productInfoFigure);
+
+    productCard.append(productCardImg, productInfo);
+
+    let cardsContainer = document.getElementById('cardsContainer');
+    cardsContainer.appendChild(productCard);
+}
