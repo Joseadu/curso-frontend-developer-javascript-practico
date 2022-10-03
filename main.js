58a55dbad267d74a1e0211592b8fe75574461a46
Jose Antonio Diaz <66831488+Joseadu@users.noreply.github.com>
2022-10-03 18:10:09 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ const productDetail = document.getElementById('productDetail');
 const navbarShoppingCart = document.getElementById('navbarShoppingCart');
 const productDetailSecondary = document.getElementById('productDetailSecondary');
 const productDetailSecondaryClose = document.getElementById('productDetailSecondaryClose');
+const openProductForm = document.getElementById('openProductForm');
+const formAddProduct = document.getElementById('formAddProduct');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 navbarShoppingCart.addEventListener('click', toggleNavbarShoppingCart);
 productDetailSecondaryClose.addEventListener('click', toggleProductDetailSecondaryClose);
+openProductForm.addEventListener('click', toggleOpenProductForm);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
@@ -37,95 +40,82 @@ function openProductDetail () {
 function toggleProductDetailSecondaryClose () {
     productDetailSecondary.classList.add('inactive');
 }
+function toggleOpenProductForm () {
+    formAddProduct.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+}
 
 
 // CREAR LISTA DE PRODUCTOS AUTOMATICAMENTE
 
-const productList = [];
+// EMPIEZA AÑADIR PRODUCTO FORM
 
-productList.push({
-    name: 'Portátil',
-    price: '1000',
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+let formBtn = document.getElementById('formBtn');
 
-productList.push({
-    name: 'Teclado',
-    price: '70',
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+formBtn.addEventListener('click', añadirProductoFunction);
 
-productList.push({
-    name: 'Ratón',
-    price: '50',
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+function añadirProductoFunction (event) {
+    function Product (title, price, image) {
+        this.title = title;
+        this.price = price;
+        this.image = image;
+    }
 
+    let titleProduct = document.getElementById('titleProduct').value;
+    let priceProduct = parseInt(document.getElementById('priceProduct').value);
+    let imageProduct = document.createElement('img');
+    imageProduct.setAttribute('src', './img/bike.jpg');
 
-// // GENERAR PRODUCTOS AUTOMÁTICAMENTE
+    let newProduct = new Product (titleProduct, priceProduct, imageProduct);
 
-// for (product of productList) {
+    const productList = [];
 
-//     const htmlCards = 
-//     `
-//     <div class="product-card">
-//         <img src=${product.image} alt="">
-//         <div class="product-info">
-//           <div>
-//             <p>$${product.price}</p>
-//             <p>${product.name}</p>
-//           </div>
+    productList.push(newProduct)
 
-//           <figure>
-//             <img src="./icons/bt_add_to_cart.svg" alt="">
-//           </figure>
-//         </div>
-//       </div>
-//     `
+    for (product of productList) {
 
-//     const cardsContainer = document.getElementById('cardsContainer');
-//     cardsContainer.innerHTML += htmlCards;
-// }
-
-for (product of productList) {
-
-    // Crear todos los atributos necesarios añadiéndoles su clase necesaria y previamente maquetadas en CSS
-
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
-
-    const productCardImg = document.createElement('img');
-    productCardImg.setAttribute('src', product.image);
-    productCardImg.addEventListener('click', openProductDetail)
-
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-
-    const productInfoDescription = document.createElement('div');
-
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
+        // Crear todos los atributos necesarios añadiéndoles su clase necesaria y previamente maquetadas en CSS
     
-    const productName = document.createElement('p');
-    productName.innerText = product.name;
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productCardImg = imageProduct;
+        imageProduct.addEventListener('click', openProductDetail)
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDescription = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = newProduct.price + '€';
+        
+        const productName = document.createElement('p');
+        productName.innerText = newProduct.title;
+    
+        const productInfoFigure = document.createElement('figure');
+    
+        const productInfoFigureImg = document.createElement('img');
+        productInfoFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+    
+    
+        // Ahora hay que asignarles a cada elemento su contenedor padre
+    
+        productInfoFigure.appendChild(productInfoFigureImg);
+    
+        productInfoDescription.append(productPrice, productName);
+    
+        productInfo.append(productInfoDescription, productInfoFigure);
+    
+        productCard.append(productCardImg, productInfo);
+    
+        let cardsContainer = document.getElementById('cardsContainer');
+        cardsContainer.appendChild(productCard);
+    }
 
-    const productInfoFigure = document.createElement('figure');
-
-    const productInfoFigureImg = document.createElement('img');
-    productInfoFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
-
-
-
-    // Ahora hay que asignarles a cada elemento su contenedor padre
-
-    productInfoFigure.appendChild(productInfoFigureImg);
-
-    productInfoDescription.append(productPrice, productName);
-
-    productInfo.append(productInfoDescription, productInfoFigure);
-
-    productCard.append(productCardImg, productInfo);
-
-    let cardsContainer = document.getElementById('cardsContainer');
-    cardsContainer.appendChild(productCard);
+    // HE AÑADIDO TIPO SUBMIT AL BOTÓN DEL FORMULARIO Y PREVENTDEFAULT A LA FUNCIÓN PARA MANDAR EL FORMULARIO Y QUE NO SE RECARGUE LA PÁGINA
+    event.preventDefault();
+    // LIMPIAR LOS INPUTS DEL FORMULARIO DESPUÉS DE ENVIAR EL FORMULARIO
+    myForm.reset();
 }
